731b95bab19e0f89dcf835115c2d6cb5673542a8
florian <florianbracq42@gmail.com>
2021-02-17 22:00:10 +0100

    Création du panier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import react from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreeen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
@@ -19,6 +20,7 @@ function App() {
               </div>
           </header>
           <main>
+            <Route path="/cart/:id?" component={CartScreen}></Route>
             <Route path="/product/:id" component={ProductScreen}></Route>
             <Route path="/" component={HomeScreeen} exact></Route>
             
