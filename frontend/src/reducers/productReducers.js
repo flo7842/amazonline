1fb97c255bb8f780b55221e4348f0e18fa70731b
florian <florianbracq42@gmail.com>
2021-02-21 19:57:22 +0100

    Fonctionnalité de supression d'un article dans le panier

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -30,4 +30,4 @@ export const productDetailsReducer = (
         default:
             return state;
     }
-}
+};
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -10,7 +10,7 @@ export default function ProductScreen(props) {
     const dispatch = useDispatch();
     const productId = props.match.params.id;
     const [qty, setQty] = useState(1);
-    const productDetails = useSelector( state => state.productDetails);
+    const productDetails = useSelector((state) => state.productDetails);
     const { loading, error, product } = productDetails;
         
     useEffect(() => {
