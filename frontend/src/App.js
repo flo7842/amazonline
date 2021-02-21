52cec96ce04bb5f9e0baa09958998d522ea7f989
florian <florianbracq42@gmail.com>
2021-02-21 10:59:17 +0100

    Ajout des fonctionnalitées du panier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 import react from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreeen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
         <div className="App">
         <div className="grid-container">
           <header className="row">
               <div>
-                  <a className="brand" href="index.html">amazona</a>
+                  <Link className="brand" to="index.html">amazona</Link>
               </div>
               <div>
-                  <a href="/cart/">Cart</a>
-                  <a href="/signin/">Sign In</a>
+                  <Link to="/cart/">Cart
+                  {cartItems.length > 0 && (
+                    <span className="badge">{cartItems.length}</span>
+                  )}</Link>
+                  <Link to="/signin/">Sign In</Link>
               </div>
           </header>
           <main>
