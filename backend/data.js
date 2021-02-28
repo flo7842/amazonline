a2c2ea9c449ff9da3e81104342b21b4cd19b0ca8
florian <florianbracq42@gmail.com>
2021-02-28 11:14:32 +0100

    Ajout de jwt

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,22 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name: 'Basir',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'John',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products:[
         {
-            _id: '1',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
             image: '/images/p1.jpg',
@@ -14,7 +29,6 @@ const data = {
 
         },
         {
-            _id: '2',
             name: 'Adidas Fit Shirt',
             category: 'Shirts',
             image: '/images/p2.jpg',
@@ -27,7 +41,6 @@ const data = {
 
         },
         {
-            _id: '3',
             name: 'Lacoste Free Shirt',
             category: 'Shirts',
             image: '/images/p3.jpg',
@@ -40,7 +53,6 @@ const data = {
 
         },
         {
-            _id: '4',
             name: 'Nike Slim Pant',
             category: 'Pants',
             image: '/images/p4.jpg',
@@ -53,7 +65,6 @@ const data = {
 
         },
         {
-            _id: '5',
             name: 'Puma Slim Pant',
             category: 'Pants',
             image: '/images/p5.jpg',
@@ -66,7 +77,6 @@ const data = {
 
         },
         {
-            _id: '6',
             name: 'Adidas Fit Pant',
             category: 'Pants',
             image: '/images/p6.jpg',
@@ -77,8 +87,7 @@ const data = {
             description: 'High quality product',
             countInStock: 30
 
-        }
-    ]
+        },
+    ],
 };
-
 export default data;
